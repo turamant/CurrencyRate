b724590d015b31e7a58f4f0022ac60e2d6446238
Victor Turamant <60545309+turamant@users.noreply.github.com>
2022-07-11 16:57:46 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-$w@=u$jg$h52hi7-y!%*s&b09*&2e367iry9@3*3q12s-di#d%'
+
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
